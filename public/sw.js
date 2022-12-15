8e8f4015317bb3e33f59f93930c48cefdf968d9a
Antonio Juričić <76628050+antoniojuricic@users.noreply.github.com>
2022-12-15 16:38:21 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,6 @@ let coreAssets = [
   "manifest.json",
   "index.html",
   "recorder.html",
-  "404.html",
-  "offline.html",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
 ];
 
