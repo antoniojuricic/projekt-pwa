a4ae08db097f665fb3d52f38a446a835a0d53214
Antonio Juričić <76628050+antoniojuricic@users.noreply.github.com>
2022-12-15 13:32:27 +0100

    folder structure

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const nodemailer = require("nodemailer");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/sendNotif", function (req, res) {
   var transporter = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
